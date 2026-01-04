6e0d5c62b39ce8ea84fa726cf66a10a5f8021f73
leo-grnd <leogarnaud84@gmail.com>
2026-01-04 13:26:10 +0100

    Configure Firebase hosting with static export. Updated `.gitignore`, added Firebase configuration files (`firebase.json`, `.firebaserc`), and static `404.html` and `index.html` templates. Adjusted `next.config.ts` for static build compatibility.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export', // Ajoute cette ligne pour forcer un site 100% statique
+    images: {
+        unoptimized: true, // Nécessaire pour l'export statique si vous utilisez <Image />
+    },
 };
 
 export default nextConfig;
